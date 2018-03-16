134a02238db1b9f689bd3d69d23df680d18a3346
Rebecca Bartels <rebeccakbartels@gmail.com>
2018-03-16 13:01:33 -0400

    established EJS files

--- app.js
+++ app.js
@@ -2,40 +2,53 @@
 var express =require('express');
 var bodyParser = require('body-parser');
 var path = require('path'); 
-
 var app = express (); 
 
-//middlewear 
-
-var logger = function(req, res, next) {
-	console.log("Logging...");
+/*var logger = function(req, res, next){
+	console.log('Logging, yo...');
 	next();
 }
 
+app.use(logger);
+*/
 
+//body parser middleware 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
-// View Engine 
+//Set static path
+app.use(express.static(path.join(__dirname, 'public')))
+
+/*var people = [
+	{
+		name:'Jeff',
+		age: 30
+	},
+	{
+		name:'Sara',
+		age:22
+	},
+	{
+		name:'Bilbo',
+		age: 35
+	}
+]
+*/
+
+//email that guy
+
+//view engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-
-//app.use(logger); //to use middlewear pass in logger 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false})); //bodyparser middlewear
-
-
-//Set static/public path
-
-app.use(express.static(path.join(__dirname, 'public')))
-
 //routes handler
 app.get('/', function(req, res){
-	
-	res.render('index', {
-		title: 'Customers'
-	}); 
+	res.render('index');
+
 });
 
-app.listen(3000,  function(){
-	console.log('Server started on Port 3000');
+
+
+app.listen(3000, function(){
+	console.log('server started on port 3000');
 })
